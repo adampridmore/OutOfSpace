08442088357d70d47e725e91482206a2b9077a02
adampridmore <320922+adampridmore@users.noreply.github.com>
2025-03-12 20:47:05 +0000

    Extracted a decompress class

---
+++ compress/Decompress.cs
@@ -0,0 +1,30 @@
+﻿namespace compress;
+
+public class Decomress()
+{
+    public void Execute()
+    {
+        var filenames = Directory
+            .GetFiles(Compress.DestinationFolder,"*.sif");
+
+        Directory.Delete(Compress.DestinationFolderDecompressed, true);
+        Directory.CreateDirectory(Compress.DestinationFolderDecompressed);
+
+        foreach (var filename in filenames)
+        {
+            var destinationFilename = Path.Combine(Compress.DestinationFolderDecompressed, Path.GetFileName(filename));
+            File.Copy(filename, destinationFilename);
+        }
+
+        var decompressionDetailsLines = File.ReadAllLines(Path.Combine(Compress.DestinationFolder, Compress.CompressDetailsFilename));
+
+        foreach (var compressionLine in decompressionDetailsLines)
+        {
+            var parts = compressionLine.Split(" ");
+            var sourceFilename = Path.Combine(Compress.DestinationFolderDecompressed, parts[0]);
+            var destinationFilename = Path.Combine(Compress.DestinationFolderDecompressed, parts[2]);
+
+            File.Copy(sourceFilename, destinationFilename);
+        }
+    } 
+}
