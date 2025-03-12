d41b380a66493af9abfea8a5ee64024c66e7bfcf
adampridmore <320922+adampridmore@users.noreply.github.com>
2025-03-12 20:22:46 +0000

    Extracted code to a class

---
+++ compress/Compress.cs
@@ -0,0 +1,88 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace compress;
+
+internal class Compress
+{
+  private const string ImagesFolder = "/Users/adampridmore/work/Dev/york-code-dojo/OutOfSpace/Images";
+  private const string DestinationFolder = "/Users/adampridmore/work/Dev/york-code-dojo/OutOfSpace/ImagesCompressed";
+
+  private static string ComputeHash(byte[] image)
+  {
+    var data = SHA256.HashData(image);
+    var sBuilder = new StringBuilder();
+    foreach (var t in data)
+      sBuilder.Append(t.ToString("x2"));
+    return sBuilder.ToString();
+  }
+
+  public void Execute()
+  {
+
+    var fileNames = Directory.GetFiles(ImagesFolder);
+    // Directory.GetFiles(destinationFolder);
+    Directory.Delete(DestinationFolder, true);
+    Directory.CreateDirectory(DestinationFolder);
+    
+    using var compressStreamWriter = File.CreateText(Path.Combine(DestinationFolder, "compress.txt"));
+
+    var fileHashes = new Dictionary<string, List<string>>();
+
+    foreach (var filename in (fileNames))
+    {
+      var fileHash = ComputeHash(File.ReadAllBytes(filename));
+      if (fileHashes.TryGetValue(fileHash, out var matchingFileNames))
+      {
+        matchingFileNames.Add(filename);
+      }
+      else
+      {
+        fileHashes.Add(fileHash, new List<string>(new[] { filename }));
+      }
+    }
+
+    foreach (var keyValue in fileHashes)
+    {
+      var fileNamesToCopy = keyValue.Value;
+
+      if (fileNamesToCopy.Count == 1)
+      {
+        var filename = fileNamesToCopy.Single();
+        var destinationFilename = Path.Combine(DestinationFolder, Path.GetFileName(filename));
+
+        File.Copy(filename, destinationFilename);
+      }
+      else
+      {
+        var filename = fileNamesToCopy.First();
+        var destinationFilename = Path.Combine(DestinationFolder, Path.GetFileName(filename));
+
+        File.Copy(filename, destinationFilename);
+
+        // TODO: Make note somewhere that there's a copy
+        foreach (var fileNameThatIsACopy in fileNamesToCopy.Skip(1))
+        {
+          var compressionLine = $"{Path.GetFileName(filename)} COPY {Path.GetFileName(fileNameThatIsACopy)} ";
+          compressStreamWriter.WriteLine(compressionLine);
+        }
+      }
+
+      // Console.WriteLine(filename);
+      //
+      // var destinationFilename = Path.Combine(destinationFolder, Path.GetFileName(filename));
+      //
+      // File.Copy(filename, destinationFilename);
+    }
+
+    foreach (var keyValue in fileHashes)
+    {
+      Console.WriteLine($"hash: {keyValue.Key} filename {keyValue.Value.Count}");
+      // foreach (var filename in keyValue.Value)
+      // {
+      //     Console.WriteLine($"hash: {keyValue.Key} filename {filename}");    
+      // }
+      // // Console.WriteLine($"hash: {keyValue.Key} filename {keyValue.Value.Count}");
+    }
+  }
+}
